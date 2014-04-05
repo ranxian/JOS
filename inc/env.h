d4ede9e9cb11771cc2d8a8a7841b882ab2caf04b
Xian Ran <xranthoar@gmail.com>
2014-04-05 10:14:26 +0000

    .

--- inc/env.h
+++ inc/env.h
@@ -68,6 +68,9 @@ struct Env {
 	uint32_t env_ipc_value;		// Data value sent to us
 	envid_t env_ipc_from;		// envid of the sender
 	int env_ipc_perm;		// Perm of page mapping received
+
+	// Lab4 challenge
+	int priority;
 };
 
 #endif // !JOS_INC_ENV_H
